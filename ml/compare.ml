60eea2363f42d238710c2dbaa0b69f2ef3a0fca6
Diao <diaozheng999@gmail.com>
2021-02-26 10:06:30 +0800

    update compare interface re

--- ml/compare.ml
+++ ml/compare.ml
@@ -1,10 +1,10 @@
 open Traits
 open Boost_internal_re
 
-external abs_int: int abs_compare = "int" [@@bs.module "../common/compare.js"]
-external abs_num: float abs_compare = "num" [@@bs.module "../common/compare.js"]
-external abs_str: string abs_compare = "str" [@@bs.module "../common/compare.js"]
-external abs_bool: bool abs_compare = "bool" [@@bs.module "../common/compare.js"]
+external int: int abs_compare = "int" [@@bs.module "../common/compare.js"]
+external num: float abs_compare = "num" [@@bs.module "../common/compare.js"]
+external str: string abs_compare = "str" [@@bs.module "../common/compare.js"]
+external bool: bool abs_compare = "bool" [@@bs.module "../common/compare.js"]
 external negate: 'a abs_compare -> 'a abs_compare = "negate" [@@bs.module "../common/compare.js"]
 
 external arr_: 'a array -> 'a array -> 'a abs_compare -> abs_ord = "arr" [@@bs.module "../common/compare.js"] [%%private]
